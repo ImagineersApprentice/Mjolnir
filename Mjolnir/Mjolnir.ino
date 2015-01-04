197b7dafa0d4999984f86eaa87218da8ced9f7ec
ImagineersApprentice <burch.emilys@gmail.com>
2015-01-04 12:13:32 -0500

    Added Sleep
    * Sleep function to save battery
    * Fixed when vibration sensor is triggered

--- Mjolnir/Mjolnir.ino
+++ Mjolnir/Mjolnir.ino
@@ -1,3 +1,4 @@
+#include <Narcoleptic.h>
 #include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
 #include <XBee.h>
@@ -29,7 +30,7 @@ Adafruit_NeoPixel tStrip = Adafruit_NeoPixel(TOTAL_LEDS, tPIN, NEO_GRB + NEO_KHZ
 /***** Vibration Sensor Setup ********/
 /*************************************/
 
-const int sensorPin =  5;      // the number of the sensor pin
+const int sensorPin =  7;      // the number of the sensor pin
 
 /********************************/
 /***** XBee Sensor Setup ********/
@@ -54,9 +55,6 @@ uint8_t rssi = 0;
 /***************************/
 const int switchWait = 2;
 const int switchOn = 3;
-int switchWaitState = 0;
-int switchOnState = 0;
-
 
 void setup(){
 
@@ -76,68 +74,73 @@ void setup(){
   //initalize Switch
   pinMode(switchWait, INPUT_PULLUP);
   pinMode(switchOn, INPUT_PULLUP);
+  digitalWrite(switchOn, HIGH);
+  digitalWrite(switchWait, HIGH);
 }
 
 void loop() {
 
-  switchWaitState = digitalRead(switchWait);
-  switchOnState = digitalRead(switchOn);
+ Narcoleptic.delay(500); // During this time power consumption is minimised
+ cd77colorallfill(200);
+ 
+  while (digitalRead(switchOn) == LOW || digitalRead(switchWait) == LOW){
 
-  if (switchWaitState == LOW){
-    xbee.readPacket();
+    if (digitalRead(switchWait) == LOW){
+      xbee.readPacket();
 
-    // check XBee on Gauntlet
-    if (xbee.getResponse().isAvailable()) {
-      // got something
+      // check XBee on Gauntlet
+      if (xbee.getResponse().isAvailable()) {
+        // got something
 
-      // create a response
-      xbee.getResponse().getRx16Response(rx16);
-      data = rx16.getData(0);
-      rssi = rx16.getRssi();
-      Serial.print("RSSI: ");
-      Serial.print(rssi);
-      Serial.println();
+        // create a response
+        xbee.getResponse().getRx16Response(rx16);
+        data = rx16.getData(0);
+        rssi = rx16.getRssi();
+        //Serial.print("RSSI: ");
+        //Serial.print(rssi);
+        //Serial.println();
+        //Serial.print("Data: ");
+        //Serial.print(data);
+        //Serial.println();        
 
-      // if Mjolnir is too far from the gauntlet 
-      //get angry
-      if (rssi >=65){
+        // if Mjolnir is too far from the gauntlet 
+        //get angry
+        if (rssi >=65){
 
-        //Mjolnir Gets Angry
-        // lightning effect
-        delay(50);
-        lightning(0.08);
-        delay(50);
-        lightning(0.175);
-        delay(50);
+          //Mjolnir Gets Angry
+          // lightning effect
+          delay(50);
+          angryLightning(0.08);
+          delay(50);
+          angryLightning(0.175);
+          delay(50);
+          cd77colorallfill(200);
+          
+          
+        }
+        // if muscle is activated
+        // pretty lights
+        else if (data == 49){
+
+        rainbow(10);
         cd77colorallfill(200);
+        
+        }
+      }
+      // check the Vibration Sensor  
+      if (digitalRead(sensorPin) == LOW){
 
-      }
-      // if muscle is activated
-      // pretty lights
-      else if (data == 1){
-      }
+      lightningOnce(100);
+      cd77colorallfill(1000); 
+        
+       
+       }
     }
-    // check the Vibration Sensor  
-    /*else if (digitalRead(sensorPin) == LOW){
-     // lightning effect
-     delay(50);
-     lightning(0.08);
-     delay(50);
-     lightning(0.175);
-     delay(50);
-     cd77colorallfill(200);
-     
-     }*/
-  }
-  else if (switchOnState == LOW){
 
-    delay(50);
-    lightningR(0.08);
-    delay(50);
-    lightningR(0.175);
-    delay(50);
-    cd77colorallfill(200);    
+    if (digitalRead(switchOn) == LOW){
+      cd77colorallfillOn(200);
 
+    }
   }
 
 
@@ -150,12 +153,37 @@ void lightning(float SpeedFactor){
     // Intensity will go from 10 - MaximumBrightness in a "breathing" manner
     float intensity = MaximumBrightness /2.0 * (1.0 + sin(SpeedFactor * i));
     lStrip.setBrightness(intensity);
+    tStrip.setBrightness(intensity);
     // Now set every LED to that color
     for (int ledNumber=0; ledNumber<TOTAL_LEDS; ledNumber++) {
       lStrip.setPixelColor(ledNumber, 88, 195, 185);
+      tStrip.setPixelColor(ledNumber, 88, 195, 185);
     }
 
     lStrip.show();
+    tStrip.show();
+    //Wait a bit before continuing to breathe
+    delay(StepDelay);
+  } 
+}
+
+
+void angryLightning(float SpeedFactor){
+
+  // Make the lights breathe
+  for (int i = 0; i < 100; i++) {
+    // Intensity will go from 10 - MaximumBrightness in a "breathing" manner
+    float intensity = MaximumBrightness /2.0 * (1.0 + sin(SpeedFactor * i));
+    lStrip.setBrightness(intensity);
+    tStrip.setBrightness(intensity);
+    // Now set every LED to that color
+    for (int ledNumber=0; ledNumber<TOTAL_LEDS; ledNumber++) {
+      lStrip.setPixelColor(ledNumber, 250, 0, 0);
+      tStrip.setPixelColor(ledNumber, 250, 0, 0);
+    }
+
+    lStrip.show();
+    tStrip.show();
     //Wait a bit before continuing to breathe
     delay(StepDelay);
   } 
@@ -164,30 +192,76 @@ void lightning(float SpeedFactor){
 // Fills the NeoMatrix 8X8 panel with all of the NeoPixes at one time 
 void cd77colorallfill(uint16_t wait) {  
   for(uint16_t i=0; i<lStrip.numPixels(); i++) {
-    lStrip.setPixelColor(i, lStrip.Color(0, 0, 0));      
+    lStrip.setPixelColor(i, lStrip.Color(0, 0, 0));
+    tStrip.setPixelColor(i, lStrip.Color(0, 0, 0));    
   }       
   lStrip.show();
+  tStrip.show();
   delay(wait);
-}    
+}
 
-void lightningR(float SpeedFactor){
+void cd77colorallfillOn(uint16_t wait) {  
+  for(uint16_t i=0; i<lStrip.numPixels(); i++) {
+    lStrip.setPixelColor(i, lStrip.Color(88, 195, 185));   
+    tStrip.setPixelColor(i, tStrip.Color(88, 195, 185));   
+  }       
+  lStrip.show();
+  tStrip.show();
+  delay(wait);
+}
+
+void lightningOnce(float SpeedFactor){
 
   // Make the lights breathe
-  for (int i = 0; i < 100; i++) {
+  for (int i = 100; i == 0; i--) {
     // Intensity will go from 10 - MaximumBrightness in a "breathing" manner
     float intensity = MaximumBrightness /2.0 * (1.0 + sin(SpeedFactor * i));
     lStrip.setBrightness(intensity);
+    tStrip.setBrightness(intensity);
     // Now set every LED to that color
     for (int ledNumber=0; ledNumber<TOTAL_LEDS; ledNumber++) {
-      lStrip.setPixelColor(ledNumber, 250, 0, 0);
+      lStrip.setPixelColor(ledNumber, 88, 195, 185);
+      tStrip.setPixelColor(ledNumber, 88, 195, 185);
     }
 
     lStrip.show();
+    tStrip.show();
     //Wait a bit before continuing to breathe
     delay(StepDelay);
   } 
 }
 
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 165 - WheelPos;
+  if(WheelPos < 85) {
+   return lStrip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return tStrip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return lStrip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return tStrip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return lStrip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return tStrip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<lStrip.numPixels(); i++) {
+      lStrip.setPixelColor(i, Wheel((i+j) & 255));
+      tStrip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    lStrip.show();
+    tStrip.show();
+    delay(wait);
+  }
+}
 
 
 
